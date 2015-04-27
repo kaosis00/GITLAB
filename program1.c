0b9e8290b9e24be88be53a5d00c9919ec1fe451e
kaosis00 <kaosis00001@gmail.com>
2015-04-27 10:25:50 +0700

    First copy of 3 files

---
+++ program1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 100;
+    int b = 200;
+    int c = 0;
+
+    printf("HELLOWORLD! \n");
+    printf("TEST PROGRAM1 \n");
+    c = a + b;
+    printf("%d",c);
+
+}
---
+++ program2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 100;
+    int b = 200;
+    int c = 0;
+
+    printf("HELLOWORLD! \n");
+    printf("TEST PROGRAM2 \n");
+    c = (a + b) * 2;
+    printf("%d",c);
+
+}
